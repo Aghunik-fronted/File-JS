2e1ffa8705f3d20767ad37fea86d587b303f0078
Aghunik <aghunik.sahakyan@mail.ru>
2026-01-28 20:57:42 +0300

    init new files

---
+++ main.js
@@ -0,0 +1,73 @@
+// console.log("Hello world");
+
+// let x = 'Hello world!!!';
+// let y = x
+// x = 'До свидания';
+// console.log(x);
+// console.log(y)
+
+// let importantAlert = 'Какое-то оповещение';
+// let isCorrect = true;
+// isCorrect = false;
+
+// const PI = 3.14;
+
+// let y = 5;
+// let z;
+// let n = null;
+// let t = true;
+// let f = false;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+//Типы данных
+/*
+  1.Текст
+  2.Числа
+  3.Underfiend - неопределённое значение
+  4. Именно 0
+  5.True/false
+  6. bigInt
+
+  */
+//  Операторы
+let x = 2;
+let y = 3;
+
+// console.log(x + y); //Сложение
+// console.log(x - y); // Вычитание
+// console.log(x * y); // Умножение
+// console.log(x / y); //Деление
+// console.log(x ** 2); // Возведение в степень
+// console.log(y % x); // Остаток
+// console.log(-x); //Смена знака
+// x++
+// console.log(++x);
+// x = 2;
+// y = 3;
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x <= y);
+// console.log(x >= y);
+// let valuePrompt = prompt('Введите ваше число', 50);
+// alert('Ваше число ' + valuePrompt);
+// console.log(+valuePrompt);
+
+
+
+// valuePrompt = +prompt('Ввведите ваше число');
+// alert(valuePrompt ** 2);
+// x = +prompt('Введите ваше число 1');
+// y = +prompt('Введите ваше число 2');
+// let avarage = (x + y) / 2;
+// alert(avarage);
+
+// x = +prompt('Введите длину стороны квадрата');
+// let S = x ** 2;
+// alert('Площадь квадрата равна' + S);
+
+let name = prompt('Введите имя');
+alert('Привет ' + name);
+name = 'Виталий'
