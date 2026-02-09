af70cd6db4826dda49441ddee471db175cab9f19
Aghunik <aghunik.sahakyan@mail.ru>
2026-02-09 21:00:49 +0300

    update

--- main.js
+++ main.js
@@ -33,8 +33,8 @@
 
   */
 //  Операторы
-let x = 2;
-let y = 3;
+// let x = 2;
+// let y = 3;
 
 // console.log(x + y); //Сложение
 // console.log(x - y); // Вычитание
@@ -68,6 +68,119 @@ let y = 3;
 // let S = x ** 2;
 // alert('Площадь квадрата равна' + S);
 
-let name = prompt('Введите имя');
-alert('Привет ' + name);
-name = 'Виталий'
+// let name = prompt('Введите имя');
+// alert('Привет ' + name);
+// name = 'Виталий'
+
+
+// Обекты
+// const objOld = new Object();
+
+// const objNew = {
+//   name: 'Никита',
+//   secondName: 'Елагин',
+//   'is admin': true,
+//   role: null,
+// };
+
+// console.log(objNew['is admin']);
+
+// objNew.age = 28;
+
+// console.log(objNew);
+
+// delete objNew.age;
+// console.log(objNew);
+// console.log(objNew.test);
+
+
+// if (objNew.name !== undefined) {
+//   console.log('Свойство существует');
+
+// }
+
+// // 'key' in obj
+
+// console.log('role' in objNew);
+
+// for (key in objNew) {
+//   console.log(key);
+//   objNew[key];
+// }
+
+// const objNewNew = {
+//   name: 'test',
+// }
+// objNewNew.secondName = 'Sahakyan';
+// objNewNew.secondName = 'NeSahakyan';
+
+// console.log(objNewNew);
+
+// function getName(name, secondName) {
+//   return {
+//     name,
+//     secondName,
+//   }
+// }
+
+// console.log(getName('Никита', 'Sahakyan'));
+
+// 1.Создать пустой объект с названием user
+// 2.Добавить свойство  name  со своим именем
+// 3. Добавить свойство  secondname
+// 4. Изменит значение name  на другое
+// 5. Удалить свойство name
+// После каждого шага  console.log()
+
+// const user = {
+
+// }
+// user.name = 'Aghunik';
+
+// user.secondName = 'Sahakyan';
+
+// user.name = 'Maxim';
+
+// delete user.name;
+
+// console.log(user);
+
+// function isEmpty(obj) {
+//   for (key in obj) {
+//     return false;
+//   }
+//   return true;
+// }
+// console.log(isEmpty({}));
+
+// let obj = {
+//   name: 'Тест',
+
+// }
+
+// Массивы
+console.log(typeof {});
+
+let arr = ['Aghunik', 'Almida', 'Albert'];
+console.log(arr.length);
+
+arr.push('Hello');
+console.log(arr);
+arr.pop();
+console.log(arr);
+
+arr.unshift('Start', 1236)
+console.log(arr);
+arr.shift();
+console.log(arr);
+
+for (item of arr) {
+  console.log(item);
+}
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(matrix[1][1]);
